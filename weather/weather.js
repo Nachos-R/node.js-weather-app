f881e69dbb8e3ea20736642f623fd0e06005deb6
Yura Rizhniv <novocaine19@gmail.com>
2018-07-07 22:23:23 +0300

    add files

---
+++ weather/weather.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const getWeather = (lat, lng, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/2cad9a00514794101b5812277e4a5e56/${lat},${lng}`,
+        json: true
+    }, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            callback(undefined, {
+                temperature: body.currently.temperature, 
+                apparentTemperature: body.currently.apparentTemperature
+            });
+        } else {
+            callback('Unable to fetch weather.');
+        }
+    });
+};
+
+module.exports.getWeather = getWeather;
